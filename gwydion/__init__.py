ffa7c94d72475d95e7d475663a0431cfdbb27d89
Keiron Pizzey <kjpizzey@gmail.com>
2014-12-21 10:59:30 +0000

    Normal object subclassed from Normal

--- gwydion/__init__.py
+++ gwydion/__init__.py
@@ -4,11 +4,11 @@ import numpy as np
 
 from .funcs.exponential import Exponential
 from .funcs.linear import Linear
-from .funcs.gaussian import Gaussian
+from .funcs.gaussian import Gaussian, Normal
 from .funcs.logarithm import Logarithm
 from .funcs.polynomial import Polynomial, Quadratic, Cubic
 from .funcs.sine import Sine
 
 from array import RandomArray
 
-__all__ = [Cubic, Exponential, Gaussian, Linear, Logarithm, Polynomial, Quadratic, RandomArray, Sine]
+__all__ = [Cubic, Exponential, Gaussian, Linear, Logarithm, Normal, Polynomial, Quadratic, RandomArray, Sine]
--- gwydion/funcs/gaussian.py
+++ gwydion/funcs/gaussian.py
@@ -2,7 +2,7 @@ from .base import random, np, plt, Base
 
 class Gaussian(Base):
 
-    def __init__(self, N=1000, a=None, b=None, c=None, d=None, xlim=(-2,2), rand=True, rand_factor=0.02, seed=None):
+    def __init__(self, N=1000, a=None, b=None, c=None, d=None, xlim=None, rand=True, rand_factor=0.02, seed=None):
         super().__init__(N=N,
                          xlim=xlim,
                          rand=rand,
@@ -23,7 +23,22 @@ class Gaussian(Base):
             else:
                 setattr(self, key, locals()[key])
 
+        if self.xlim is None:
+            n = 10
+            self.xlim = (self.b - 4*self.c, self.b + 4*self.c)
+
     def func(self, x):
         a, b, c, d = self.a, self.b, self.c, self.d
 
         return a * np.exp(-(x - b)**2/(2*c**2)) + d
+
+class Normal(Gaussian):
+
+    def __init__(self, N=1000, mu=None, sigma=None, xlim=(-2,2), rand=True, rand_factor=0.02, seed=None):
+
+        a = 1 / (sigma*np.sqrt(2*np.pi)) if sigma is not None else None
+        b = mu
+        c = sigma
+        d = 0
+
+        super().__init__(N=N, a=a, b=b, c=c, d=d, xlim=xlim, rand=rand, rand_factor=rand_factor, seed=seed)
