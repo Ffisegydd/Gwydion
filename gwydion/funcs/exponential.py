fd5d0c94ef53c0fa46f31b50210de0c2e66b36e7
Keiron Pizzey <kjpizzey@gmail.com>
2015-03-18 20:00:11 +0000

    Updated Exponential to use better notation

--- gwydion/funcs/exponential.py
+++ gwydion/funcs/exponential.py
@@ -5,7 +5,7 @@ class Exponential(Base):
     """
     Exponential function (power law). Returned function is
 
-        y = a * base**(b*x) + c
+        y = I * base**(b*x)
 
     Parameters
     ----------
@@ -14,12 +14,10 @@ class Exponential(Base):
         Length of arrays to be returned via the data method. Defaults to 100.
     base : Float, integer, or None
         Base for function. If None, defaults to e = 2.718....
-    a : Float, integer, or None.
-        Amplitude of function. If None, defaults to a random value around 1.0.
-    b : Float, integer, or None.
+    I : Float, integer, or None.
+        Intensity of function. If None, defaults to a random value around 1.0.
+    k : Float, integer, or None.
         Amplitude of the exponent. If None, defaults to a random value around 0.0
-    c : Float, integer, or None.
-        Offset of exponent. If None, defaults to a random value around 0.0.
     xlim : Tuple of floats or integers.
         (Min, Max) values for the x-data. Defaults to (-10, 10).
     rand : Boolean.
@@ -34,27 +32,26 @@ class Exponential(Base):
 
     >>>> Exponential()  # Default params, returns a "normal" exponential.
     >>>> Exponential(N=1000)  # Increase the number of data points.
-    >>>> Exponential(b=-1, c=0)  # Exponential decay.
+    >>>> Exponential(b=-np.e)  # Exponential decay.
     >>>> Exponential(rand=False)  # Turn off randomness.
     >>>> Exponential(seed=1234)  # Seeded RNG
 
     """
 
-    def __init__(self, N=100, base=None, a=None, b=None, c=None, xlim=(-10, 10), add_rand=True, rand_factor=0.1, seed=None):
+    def __init__(self, N=100, base=None, I=None, k=None, xlim=(-10, 10), add_rand=True, rand_factor=0.1, seed=None):
         super().__init__(N=N,
                          xlim=xlim,
                          add_rand=add_rand,
                          rand_factor=rand_factor,
                          seed=seed)
 
-        self.set_variables(base, a, b, c)
+        self.set_variables(base, I, k)
 
-    def set_variables(self, base, a, b, c):
+    def set_variables(self, base, I, k):
 
         defaults = {'base': np.e,
-                    'a': 1.0 + (self.random.rand() - 0.5) * 0.5,
-                    'b': (self.random.rand() - 0.5) * 0.5,
-                    'c': (self.random.rand() - 0.5) * 0.5}
+                    'I': 1.0 + (self.random.rand() - 0.5) * 0.5,
+                    'k': (self.random.rand() - 0.5) * 0.5}
 
         for key, val in defaults.items():
             if locals()[key] is None:
@@ -63,6 +60,6 @@ class Exponential(Base):
                 setattr(self, key, locals()[key])
 
     def func(self, x):
-        a, b, c, base = self.a, self.b, self.c, self.base
+        I, k, base = self.I, self.k, self.base
 
-        return a * np.power(base, self.b * x) + self.c
+        return I * np.power(base, self.k * x)
