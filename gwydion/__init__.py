23c6e7560a3566eefbbd95e52c7b5dfff5d8ca27
Keiron Pizzey <kjpizzey@gmail.com>
2015-03-18 19:33:30 +0000

    Move and rename Gaussian to NormalDistribution

--- gwydion/__init__.py
+++ gwydion/__init__.py
@@ -2,12 +2,14 @@ __version__ = '0.1.dev'
 
 from gwydion.funcs.exponential import Exponential
 from gwydion.funcs.linear import Linear
-from gwydion.funcs.gaussian import Gaussian, Normal
 from gwydion.funcs.logarithm import Logarithm
 from gwydion.funcs.polynomial import Polynomial, Quadratic, Cubic
 from gwydion.funcs.sine import Sine
 
+from gwydion.stats.normal import NormalDistribution
+
 from .random_array import RandomArray
 
-__all__ = ['Cubic', 'Exponential', 'Gaussian', 'Linear', 'Logarithm',
-           'Normal', 'Polynomial', 'Quadratic', 'RandomArray', 'Sine']
+__all__ = ['Cubic', 'Exponential', 'Linear', 'Logarithm',
+           'Polynomial', 'Quadratic', 'RandomArray', 'Sine',
+           'NormalDistribution']
