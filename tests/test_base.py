8898601615b456ed89a46dff27060491641134a4
Keiron Pizzey <kjpizzey@gmail.com>
2015-03-21 15:33:17 +0000

    Further base class testing

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,24 +1,28 @@
 import pytest
-import numpy as np
 
 from gwydion.base import Base
 from gwydion.exceptions import GwydionError
 
+#TODO Not happy with this mock testing process.
+
 class MockBaseClass(Base):
-    def __init__(self, N=100, a=None, b=None, c=None, xlim=(0, 10), add_rand=True, rand_factor=0.5, seed=None):
+    def __init__(self, N=100, a=None, b=None, xlim=(0, 10), add_rand=True, rand_factor=0.5, seed=None):
         super().__init__(N=N,
                          xlim=xlim,
                          add_rand=add_rand,
                          rand_factor=rand_factor,
                          seed=seed)
 
-        self.set_variables(a, b, c)
+        self.set_variables(a, b)
 
-    def set_variables(self, a, b, c):
-        pass
+    def set_variables(self, a, b):
+        self.a, self.b = a, b
 
     def func(self, x):
-        pass
+        return self.a * self.b * x
+
+def test_base_creation():
+    x = MockBaseClass()
 
 def test_base_exceptions():
     with pytest.raises(GwydionError):
@@ -27,11 +31,13 @@ def test_base_exceptions():
     with pytest.raises(GwydionError):
         z = MockBaseClass(xlim=(0, 10, 20))
         x, y = z.data
-
     with pytest.raises(GwydionError):
         z = MockBaseClass(xlim='12345')
         x, y = z.data
 
     with pytest.raises(GwydionError):
         z = MockBaseClass(N='asesd')
+        x, y = z.data
+    with pytest.raises(GwydionError):
+        z = MockBaseClass(N=(100,100))
         x, y = z.data
