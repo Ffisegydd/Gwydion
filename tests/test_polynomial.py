55016f550981ce25c5d0972f2905813179a095e8
bluepizzey <kjpizzey@gmail.com>
2015-03-29 15:36:44 +0100

    Added Quadratic and Cubic testing

--- tests/test_polynomial.py
+++ tests/test_polynomial.py
@@ -65,9 +65,132 @@ def test_polynomial_seeding():
 
     assert all(np.array_equal(i, j) for i, j in zip(polynomial1.data, polynomial2.data))
 
+
 def test_polynomial_exceptions():
     with pytest.raises(GwydionError):
         Polynomial(a=[2j, 3j])
     with pytest.raises(GwydionError):
         Polynomial(a='1234')
 
+
+def test_quadratic_creation():
+    quadratic = Quadratic()
+    assert quadratic
+
+
+def test_quadratic_non_random():
+    quadratic = Quadratic(rand_factor=None, a=2, b=1, c=0, xlim=(-3, 3), N=7)
+    polynomial = Polynomial(rand_factor=None, a=[0, 1, 2], xlim=(-3, 3), N=7)
+
+    for u, v in zip(quadratic.data, polynomial.data):
+        for i, j in zip(u, v):
+            assert abs(i - j) < TOLERANCE
+
+
+def test_quadratic_random():
+    x_test = [-3, -2, -1, 0, 1, 2, 3]
+    y_test = [-0.3713085, 0.38057779, 0.28028819, 0.99593501, 0.57225677, 1.61515779, 1.99664164]
+
+    quadratic = Quadratic(seed=SEED, N=7, xlim=(-3, 3))
+    x, y = quadratic.data
+
+    for i, j in zip(quadratic.a, [0.13858659, 0.35275832]):
+        assert abs(i - j) < TOLERANCE
+
+    for i, j in zip(x, x_test):
+        assert abs(i - j) < TOLERANCE
+
+    for i, j in zip(y, y_test):
+        assert abs(i - j) < TOLERANCE
+
+
+def test_quadratic_printing():
+    quadratic = Quadratic(seed=SEED, N=11)
+    print(str(quadratic))
+    print(repr(quadratic))
+    for s in ['N=11', 'rand_factor=1.0']:
+        assert s in str(quadratic)
+
+    for s in ['N=11', 'xlim=(-10, 10)', 'seed=31415927', 'a=[ 0.13858659  0.35275832]', 'rand_factor=1.0']:
+        assert s in repr(quadratic)
+
+
+def test_quadratic_seeding():
+    quadratic1 = Quadratic(seed=SEED, xlim=(1,10))
+    quadratic2 = Quadratic(seed=SEED, xlim=(1,10))
+
+    assert quadratic1 != quadratic2
+    for i, j in zip(quadratic1.a, quadratic2.a):
+        assert abs(i - j) < TOLERANCE
+
+    print(quadratic1.data, quadratic2.data)
+
+    assert all(np.array_equal(i, j) for i, j in zip(quadratic1.data, quadratic2.data))
+
+def test_quadratic_exceptions():
+    with pytest.raises(GwydionError):
+        Quadratic(a=2j, b=3j)
+    with pytest.raises(GwydionError):
+        Quadratic(a='1234')
+
+
+def test_cubic_creation():
+    cubic = Cubic()
+    assert cubic
+
+
+def test_cubic_non_random():
+    cubic = Cubic(rand_factor=None, a=2, b=1, c=0, d=-1, xlim=(-3, 3), N=7)
+    polynomial = Polynomial(rand_factor=None, a=[-1, 0, 1, 2], xlim=(-3, 3), N=7)
+
+    for u, v in zip(cubic.data, polynomial.data):
+        for i, j in zip(u, v):
+            assert abs(i - j) < TOLERANCE
+
+
+def test_cubic_random():
+    x_test = [-3, -2, -1, 0, 1, 2, 3]
+    y_test = [1.82221094, 4.1706091, 2.25812782, 4.4253287, 0.89590419, 4.69937605, 5.19576204]
+
+    cubic = Cubic(seed=SEED, N=7, xlim=(-3, 3))
+    x, y = cubic.data
+    print(cubic.a)
+
+    for i, j in zip(cubic.a, [0.13858659, 0.35275832]):
+        assert abs(i - j) < TOLERANCE
+
+    for i, j in zip(x, x_test):
+        assert abs(i - j) < TOLERANCE
+
+    for i, j in zip(y, y_test):
+        assert abs(i - j) < TOLERANCE
+
+
+def test_cubic_printing():
+    cubic = Cubic(seed=SEED, N=11)
+    print(str(cubic))
+    print(repr(cubic))
+    for s in ['N=11', 'rand_factor=5.0']:
+        assert s in str(cubic)
+
+    for s in ['N=11', 'xlim=(-10, 10)', 'seed=31415927', 'a=[ 0.13858659  0.35275832]', 'rand_factor=5.0']:
+        assert s in repr(cubic)
+
+
+def test_cubic_seeding():
+    cubic1 = Cubic(seed=SEED, xlim=(1,10))
+    cubic2 = Cubic(seed=SEED, xlim=(1,10))
+
+    assert cubic1 != cubic2
+    for i, j in zip(cubic1.a, cubic2.a):
+        assert abs(i - j) < TOLERANCE
+
+    print(cubic1.data, cubic2.data)
+
+    assert all(np.array_equal(i, j) for i, j in zip(cubic1.data, cubic2.data))
+
+def test_cubic_exceptions():
+    with pytest.raises(GwydionError):
+        Cubic(a=2j, b=3j)
+    with pytest.raises(GwydionError):
+        Cubic(a='1234')
